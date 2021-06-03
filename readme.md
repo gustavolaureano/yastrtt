e8c7c38f7f1fc011fe2152c8a01061322be7c37e
Gustavo <gustavo@pop-os.localdomain>
2021-06-03 18:26:30 +0200

    First fully functional version, supporting TX and RX

--- readme.md
+++ readme.md
@@ -1,15 +1,24 @@
 YASTRTT: Yet Another ST RTT
 
 A tool to interact with the Segger RTT library through ST-Link debugger interfaces and using the the open source version of the STMicroelectronics STLINK Tools.
+(https://github.com/stlink-org/stlink)
 
 Main targets:
- - Have a single and portable binary with all the necessary libraries statically linked, removing the requirement of installing any other software (besides the own ST-Link driver)
+ - Have a single and portable binary with all the necessary libraries statically linked, removing the requirement of installing any other software but the ST-Link driver
  - Be a simple command line tool, just execute it and it will open as a terminal for you, letting you interact with the target over RTT
  - Be self-recoverable, if the connection to the target or to the ST-Link is lost the application automatically restart the search 
- 
- 
+  
  
 The official Segger RTT software supports multiple data channels in both directions, the RTT Viewer only works with channel 0 but it is able to divide the incomming data into up to 16 different virtual terminals (the terminal is selected by a special sequence of characters sent by the target), also supporting different text colors.
 This application has no intention of supporting more than one channel nor more than one "terminal", every data sent by the target over channel 0 will be directly printed to the console, and every character typed by the user will be written into the rx channel 0 on the target, if the target send special commands to change the terminal or text color these commands will be interpreted as text and printed to the console too.
 
-This code is based on (and use parts of) the rtt_stlink project (https://github.com/trlsmax/rtt_stlink)
+This code is based on (and use parts of):
+ - Segger's RTT target library (https://www.segger.com/products/debug-probes/j-link/technology/about-real-time-transfer/)
+ - the rtt_stlink project (https://github.com/trlsmax/rtt_stlink)
+ - the "Build Your Own Text Editor" booklet (https://viewsourcecode.org/snaptoken/kilo/index.html)
+
+
+Possible improvements:
+ - Integrate libstlink and libusb as submodules and automatic compilation
+ - Add command line options to allow custom CB identifiers and customizable up/down channels 
+ - Move to C++?
--- src/yastrtt.c
+++ src/yastrtt.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <termios.h>
 
 #include <stlink.h>
 
@@ -37,8 +38,13 @@ rtt_cb_t rtt_cb = {0};
 
 int capt_signal = 0;
 
+struct termios orig_termios;
+
 const char anim[4] = {'|', '/', '-', '\\'};
 int anim_index = 0;
+
+char txbuff[128];
+int txbuff_len = 0;
 
 int close_device(void)
 {
@@ -95,6 +101,9 @@ int write_mem(uint8_t *buf, uint32_t addr, uint32_t len)
     return 0;
 }
 
+/* buf = pointer to destination buffer
+ * rtt_c = pointer to the updated copy of the channel ringbuffer control block
+ * rtt_channel_addr = memory address on the target where the ringbuffer control block is located */
 int get_channel_data(uint8_t *buf, rtt_channel *rtt_c, uint32_t rtt_channel_addr)
 {
     uint32_t len;
@@ -122,31 +131,84 @@ int get_channel_data(uint8_t *buf, rtt_channel *rtt_c, uint32_t rtt_channel_addr
     }
 }
 
-int Collect_RX()
+/* txbuffer = pointer to the source data
+ * txlength = length of the source data
+ * rtt_c = pointer to the updated copy of the channel ringbuffer control block
+ * rtt_channel_addr = memory address on the target where the ringbuffer control block is located */
+int write_channel_data(char *txbuffer, int txlength, rtt_channel *rtt_c, uint32_t rtt_channel_addr)
 {
-    uint8_t buf[1024];
-    char str_buf[1024];
-    int line_cnt;
+    int original_len = txlength;
 
-    // update SEGGER_RTT_CB content
-    read_mem(buf, rtt_cb.cb_addr + 24, rtt_cb.cb_size - 24);
-    for (uint32_t j = 0; j < rtt_cb.MaxNumUpBuffers * sizeof(rtt_channel); j++)
-        ((uint8_t *)rtt_cb.aUp)[j] = buf[j];
-    for (uint32_t j = 0; j < rtt_cb.MaxNumDownBuffers * sizeof(rtt_channel); j++)
-        ((uint8_t *)rtt_cb.aDown)[j] = buf[rtt_cb.MaxNumUpBuffers * sizeof(rtt_channel) + j];
+    if (txlength == 0)
+        return 0;
 
-    memset(buf, '\0', 1024);
-    if (get_channel_data(buf, &rtt_cb.aUp[0], rtt_cb.cb_addr + 24) > 0)
+    /* we are ahead of the reading pointer, we are only limited by the size of the buffer */
+    if (rtt_c->WrOff >= rtt_c->RdOff)
     {
-        if (strlen(buf) > 0)
+        int to_write = txlength;
+        int avail = rtt_c->SizeOfBuffer - rtt_c->WrOff;
+
+        if (to_write > avail)
         {
-            strcpy(str_buf, buf);
-            printf("%s", str_buf);
-            fflush(stdout);
-            // IupSetAttribute(txt_message, "APPEND", str_buf);
-            // line_cnt = IupGetInt(txt_message, "LINECOUNT");
-            // IupSetStrf(txt_message, "SCROLLTO", "%d:1", line_cnt);
+            to_write = avail;
         }
+
+        write_mem((uint8_t *)txbuffer, rtt_c->pBuffer + rtt_c->WrOff, to_write);
+        txbuffer += to_write;
+        txlength -= to_write;
+        rtt_c->WrOff = (rtt_c->WrOff + to_write) % rtt_c->SizeOfBuffer; /* wrap to 0 if offset = size */
+    }
+
+    if ((txlength > 0) && (rtt_c->WrOff < rtt_c->RdOff))
+    {
+        int to_write = txlength;
+        int avail = (rtt_c->RdOff - rtt_c->WrOff) -1;
+
+        if (to_write > avail)
+        {
+            to_write = avail;
+        }
+
+        write_mem((uint8_t *)txbuffer, rtt_c->pBuffer + rtt_c->WrOff, to_write);
+        txbuffer += to_write;
+        txlength -= to_write;
+        rtt_c->WrOff += to_write;
+    }
+
+    /* update the write offset on the target */
+    write_mem((uint8_t *)&(rtt_c->WrOff), rtt_channel_addr + (4 * 3), 4); 
+
+    /* return the number of written bytes */
+    return (original_len - txlength); 
+}
+
+int Run_TXRX()
+{
+    uint8_t rxbuf[1024];
+
+    /* update local copy of all ring-buffers control blocks */
+    read_mem(rxbuf, rtt_cb.cb_addr + 24, rtt_cb.cb_size - 24); /* 24 = cb name (16 bytes) + MaxNumUpBuffers (uint32, 4 bytes) + MaxNumDownBuffers (uint32, 4 bytes) = rbcb start */
+    memcpy(rtt_cb.aUp, rxbuf, rtt_cb.MaxNumUpBuffers * sizeof(rtt_channel));
+    memcpy(rtt_cb.aDown, rxbuf + (rtt_cb.MaxNumUpBuffers * sizeof(rtt_channel)), rtt_cb.MaxNumDownBuffers * sizeof(rtt_channel));
+
+    memset(rxbuf, '\0', 1024);
+
+    /* the target's RAM address of the ringbuffer control block aUp[0] is the offset to the rbcb arrays */
+    if (get_channel_data(rxbuf, &rtt_cb.aUp[0], rtt_cb.cb_addr + 24) > 0)
+    {
+        printf("%s", rxbuf);
+        fflush(stdout);
+    }
+
+    if (txbuff_len > 0)
+    {
+        /* the target's RAM address of the ringbuffer control block aDown[0] is the offset to the rbcb arrays + the length of the aUp array of rbcb */
+        write_channel_data(txbuff, txbuff_len, &rtt_cb.aDown[0], rtt_cb.cb_addr + 24 + (rtt_cb.MaxNumUpBuffers * sizeof(rtt_channel)));
+        txbuff_len = 0;
+        /* We are not dealing with remaining data that was not sent to the target and we just throw it away, this should not be an 
+         * issue during normal usage as this data is only slow keyboard input and the target will likely consume it fast enough,
+         * so the down buffer (H -> T) will never be full.
+         * If necessary, the data input could be reimplemented with a ringbuffer, which would allow partial transfers to the target */
     }
 
     return 0;
@@ -261,9 +323,27 @@ void locate_rtt_cb(void)
     free(buf);
 }
 
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON); /* disable echo and canonical mode */
+    raw.c_cc[VMIN] = 0;  /* min number of chars that read() can return */
+    raw.c_cc[VTIME] = 0; /* timeout for read() */
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(int ac, char **av)
 {
     signal(SIGINT, handle_sigint);
+
+    enableRawMode();
 
     while (1)
     {
@@ -279,16 +359,29 @@ int main(int ac, char **av)
             break;
         }
 
+        /* raw input capture from terminal */
+        char c;
+        while (read(STDIN_FILENO, &c, 1) == 1)
+        {
+            if (txbuff_len < sizeof(txbuff))
+            {
+                txbuff[txbuff_len++] = c;
+            }
+        }
+
         if (open_device() == 0)
         {
             if (rtt_cb.cb_addr == 0)
             {
                 locate_rtt_cb();
+
+                /* We ignore anything that was input by the user while no RTT was available */
+                txbuff_len = 0;
             }
 
             if (rtt_cb.cb_addr != 0)
             {
-                Collect_RX();
+                Run_TXRX();
             }
         }
         else
